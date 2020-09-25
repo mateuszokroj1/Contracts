24c49a2d031bb47085957d0b8372d5479e4cca57
Mateusz Okrój <mateuszokroj1@gmail.com>
2020-09-25 16:14:36 +0200

    Added tests project.

---
+++ Contracts/Tests/Contracts.Core.Tests/ContractTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Xunit;
+
+namespace Contracts.Tests
+{
+    public class ContractTest
+    {
+        #region Constructors
+
+        [Fact]
+        public void Constructor1_WhenArgumentIsNull_ShouldThrowArgumentNullException()
+        {
+            Func<bool> predicate = () => true, nullPredicate = null;
+            Action<object> onFailure = args => predicate(), nullOnFailure = null;
+
+            Assert.Throws<ArgumentNullException>(() => new Contract(nullPredicate, onFailure));
+            Assert.Throws<ArgumentNullException>(() => new Contract(predicate, nullOnFailure));
+        }
+
+        [Fact]
+        public void Constructor1_WhenArgumentIsValid_ShouldSetProperties()
+        {
+            Func<bool> predicate = () => true;
+            Action<object> onFailure = args => predicate();
+        }
+
+        #endregion
+    }
+}
