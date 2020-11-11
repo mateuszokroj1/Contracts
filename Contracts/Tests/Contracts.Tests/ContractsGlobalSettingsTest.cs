0ae0daeea69240290ae4e34af9b69aff934e6452
Mateusz Okrój <mateuszokroj1@hotmail.com>
2020-11-11 23:58:30 +0100

    Added tests for ContractGlobalSettings, StrategyParameters.

---
+++ Contracts/Tests/Contracts.Tests/ContractsGlobalSettingsTest.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace Contracts.Tests
+{
+    public class ContractsGlobalSettingsTest
+    {
+        [Fact]
+        public void UseDebugModeWhenThrowException_ShouldSetAndGetTheSameValue()
+        {
+            ContractsGlobalSettings.UseDebugModeWhenThrowException = false;
+
+            var result1 = ContractsGlobalSettings.UseDebugModeWhenThrowException;
+
+            ContractsGlobalSettings.UseDebugModeWhenThrowException = true;
+
+            Assert.False(result1);
+            Assert.True(ContractsGlobalSettings.UseDebugModeWhenThrowException);
+        }
+    }
+}
---
+++ Contracts/Tests/Contracts.Tests/Models/StrategyParametersTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Contracts.Models;
+
+using Xunit;
+
+namespace Contracts.Tests.Models
+{
+    public class StrategyParametersTest
+    {
+        #region Constructor
+
+        [Fact]
+        public void Constructor_ShouldNotThrow()
+        {
+            var result = new StrategyParameters();
+
+            Assert.NotNull(result);
+        }
+
+        #endregion
+
+        #region Properties
+
+        [Fact]
+        public void Message_ShouldSetAndGetTheSameValue()
+        {
+            var parameters = new StrategyParameters();
+            parameters.Message = null;
+
+            var result1 = parameters.Message;
+
+            parameters.Message = "TEST";
+
+            Assert.Equal("TEST", parameters.Message);
+        }
+
+        #endregion
+    }
+}
